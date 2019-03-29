10ab8839d1c79a6485b98b61627b77e730d1acdb
Dominik Rolewski <drolewski@gmail.com>
2019-03-29 17:40:00 +0100

    Create handle of Matrix getter.

--- src/com/rolex/UI/Controller.java
+++ src/com/rolex/UI/Controller.java
@@ -1,4 +1,18 @@
 package com.rolex.UI;
 
+import com.rolex.datamodel.Matrix;
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+
 public class Controller {
+
+    @FXML
+    private Spinner matrixSize;
+
+    @FXML
+    public void getMatrixSize(){
+        Matrix matrix = new Matrix((int) matrixSize.getValue());
+        System.out.println(matrix.getMatrixSize());
+    }
+
 }
