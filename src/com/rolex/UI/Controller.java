e43d61e3d4b741ca88cf67395c5ecbd4bd10dc35
Dominik Rolewski <drolewski@gmail.com>
2019-04-04 13:28:57 +0200

    Set TextFields as fields with prompt Text.

--- src/com/rolex/UI/Controller.java
+++ src/com/rolex/UI/Controller.java
@@ -1,11 +1,14 @@
 package com.rolex.UI;
 
 import com.rolex.datamodel.Matrix;
+import javafx.beans.DefaultProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -29,21 +32,28 @@ public class Controller {
     @FXML
     public void getMatrixSize(){
 
-
+//      size clear the gridPane and set the size of gaps between element
         equationGridPane.getChildren().clear();
         mainWindow.getChildren().remove(equationGridPane);
+        equationGridPane.setVgap(7);
+        equationGridPane.setHgap(7);
 
         int sizeOfMatrix = (int) matrixSize.getValue();
         Matrix matrix = new Matrix(sizeOfMatrix);
 
         System.out.println(matrix.getMatrixSize());
 
+//        create fields with declared size to input our matrix data
         ArrayList<TextField> textFields = new ArrayList<>();
-        for(int i = 0 ; i < sizeOfMatrix*sizeOfMatrix ; i++ ){
-            textFields.add(new TextField("Coefficient " + i));
+        for(int i = 0 ; i < sizeOfMatrix*sizeOfMatrix ; i++ ) {
+            TextField textElement = new TextField();
+            textElement.setPromptText("Coefficient " + i);
+            textElement.setMaxSize(100, 400);
+            textFields.add(textElement);
         }
 
 
+//        generate elements to the UI layout
         Label equalSign = new Label("=");
         int elementsCounter = 0;
         for(int i = 0 ; i < sizeOfMatrix ; i++){
@@ -53,8 +63,12 @@ public class Controller {
                     textFields.get(elementsCounter).requestFocus();
                 }
             }
-            equationGridPane.add(new TextField("Result " + i), 2 + sizeOfMatrix , 1 + i);
-            if(i == (int) sizeOfMatrix/2){
+            TextField resultField = new TextField();
+            resultField.setPromptText("Result " + i);
+            resultField.setMaxSize(100,400);
+            equationGridPane.add(resultField, 2 + sizeOfMatrix , 1 + i);
+
+            if(i == sizeOfMatrix/2){
                 equationGridPane.add(equalSign,1 + sizeOfMatrix , i + 1);
             }
         }
